3a6e8d1ca8f8ab49bffd5bbcc4f9cd9b01521bdb
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-28 19:00:15 +0700

    feat: pembaruan fitur app/Models/Kecamatan.php

--- app/Models/Kecamatan.php
+++ app/Models/Kecamatan.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Kecamatan extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['kode_kecamatan', 'nama_kecamatan'];
+    public function desas() { return $this->hasMany(Desa::class); }
+    
 }
