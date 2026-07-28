bf2e7155926c15ab26336aaa597f21346bf9f588
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-28 19:00:15 +0700

    feat: pembaruan fitur app/Models/Desa.php

--- app/Models/Desa.php
+++ app/Models/Desa.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Desa extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['kecamatan_id', 'nama_desa'];
+    public function kecamatan() { return $this->belongsTo(Kecamatan::class); }
+    
 }
