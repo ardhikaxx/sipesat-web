04a5c06588fcba8452baecab64131edb8143dbd7
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-28 19:00:16 +0700

    feat: pembaruan fitur app/Models/Rating.php

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Rating extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['laporan_sampah_id', 'user_id', 'rating', 'komentar'];
+    public function laporanSampah() { return $this->belongsTo(LaporanSampah::class); }
+    public function user() { return $this->belongsTo(User::class); }
+    
 }
