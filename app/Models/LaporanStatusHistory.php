4cf98828cf342a190baf1fbb540f3a2867b89187
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-28 19:00:20 +0700

    feat: pembaruan fitur app/Models/LaporanStatusHistory.php

---
+++ app/Models/LaporanStatusHistory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class LaporanStatusHistory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['laporan_sampah_id', 'status_sebelum', 'status_sesudah', 'keterangan', 'changed_by', 'changed_at'];
+    protected $casts = ['changed_at' => 'datetime'];
+    public function laporanSampah() { return $this->belongsTo(LaporanSampah::class); }
+    public function changedBy() { return $this->belongsTo(User::class, 'changed_by'); }
+    
+}
