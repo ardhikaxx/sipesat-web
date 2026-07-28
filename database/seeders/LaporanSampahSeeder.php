8ad51ef4ad8b9be48f9e9a27b2965b398ff894b6
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-28 19:09:24 +0700

    feat: memperbanyak seeder laporan menjadi 35 titik untuk visualisasi yang lebih nyata

--- database/seeders/LaporanSampahSeeder.php
+++ database/seeders/LaporanSampahSeeder.php
@@ -15,47 +15,57 @@ class LaporanSampahSeeder extends Seeder
     {
         $roleMasyarakat = Role::where('name', 'masyarakat')->first();
         
-        // Buat beberapa akun masyarakat tambahan
+        // Buat 10 akun masyarakat
         $users = [];
-        for ($i = 1; $i <= 5; $i++) {
+        for ($i = 1; $i <= 10; $i++) {
             $users[] = User::create([
                 'name' => 'Warga Magetan ' . $i,
                 'email' => 'warga' . $i . '@example.com',
                 'password' => Hash::make('password'),
                 'role_id' => $roleMasyarakat->id,
-                'phone' => '0812345600' . $i,
+                'phone' => '0812345600' . str_pad($i, 2, '0', STR_PAD_LEFT),
                 'is_active' => true,
             ]);
         }
 
-        // Kordinat yang difokuskan pada area pusat Kota Magetan agar tidak terlalu menyebar
-        $locations = [
-            ['lat' => -7.6531, 'lng' => 111.3284, 'judul' => 'Sampah Sisa Acara di Alun-Alun', 'alamat' => 'Alun-Alun Kabupaten Magetan'],
-            ['lat' => -7.6500, 'lng' => 111.3250, 'judul' => 'Sampah Sayuran Menggunung di Belakang Pasar', 'alamat' => 'Pasar Sayur Magetan'],
-            ['lat' => -7.6580, 'lng' => 111.3320, 'judul' => 'Tumpukan Botol Plastik di Area Stadion', 'alamat' => 'Kawasan Stadion Yosonegoro'],
-            ['lat' => -7.6565, 'lng' => 111.3225, 'judul' => 'Pembuangan Liar Dekat RSUD', 'alamat' => 'Jalan sekitar RSUD dr. Sayidiman'],
-            ['lat' => -7.6610, 'lng' => 111.3265, 'judul' => 'Saluran Air Tersumbat Sampah Daun', 'alamat' => 'Sekitar Taman Ria Manunggal'],
+        // List judul dan alamat realistis (acak)
+        $judulList = [
+            'Tumpukan Sampah Plastik', 'Sampah Sisa Acara', 'Sampah Sayuran Menggunung', 
+            'Pembuangan Liar', 'Saluran Air Tersumbat Sampah', 'Sampah Popok Bayi di Pinggir Jalan',
+            'Sisa Material Bangunan Menutupi Jalan', 'Tumpukan Daun Kering dan Plastik',
+            'Sampah Rumah Tangga Menumpuk', 'Kantong Sampah Tidak Terangkut'
+        ];
+        $alamatList = [
+            'Jalan Raya Magetan', 'Area Alun-Alun', 'Pasar Sayur Magetan', 'Kawasan Stadion Yosonegoro',
+            'Jalan sekitar RSUD', 'Sekitar Taman Ria Manunggal', 'Area Pemukiman Ringinagung',
+            'Dekat Terminal Baru', 'Jalan Tembus Sarangan', 'Kawasan Industri Kecil'
         ];
 
-        foreach ($locations as $index => $loc) {
+        // Generate 35 Laporan acak di sekitar Magetan
+        // Magetan Center: Lat: -7.6531, Lng: 111.3284
+        for ($i = 1; $i <= 35; $i++) {
+            // Generate random coordinate nearby (radius ~3-5km)
+            $lat = -7.6531 + (mt_rand(-300, 300) / 10000); // -7.6231 to -7.6831
+            $lng = 111.3284 + (mt_rand(-300, 300) / 10000); // 111.2984 to 111.3584
+
             LaporanSampah::create([
-                'kode_laporan' => 'SPT-20260728-900' . $index,
-                'user_id' => $users[$index]->id,
-                'kategori_sampah_id' => rand(1, 3), // Asumsi ID 1-3 valid
-                'kecamatan_id' => 1, // Magetan
-                'desa_id' => 1, 
-                'judul_laporan' => $loc['judul'],
+                'kode_laporan' => 'SPT-' . date('Ymd') . '-' . str_pad($i, 4, '0', STR_PAD_LEFT),
+                'user_id' => $users[array_rand($users)]->id,
+                'kategori_sampah_id' => rand(1, 3),
+                'kecamatan_id' => 1, 
+                'desa_id' => rand(1, 5), 
+                'judul_laporan' => $judulList[array_rand($judulList)] . ' - Titik ' . $i,
                 'deskripsi' => 'Ditemukan banyak tumpukan sampah yang belum dibersihkan dan menimbulkan bau tidak sedap.',
-                'alamat_lengkap' => $loc['alamat'],
-                'latitude' => $loc['lat'],
-                'longitude' => $loc['lng'],
+                'alamat_lengkap' => $alamatList[array_rand($alamatList)] . ' Blok ' . chr(rand(65, 90)),
+                'latitude' => $lat,
+                'longitude' => $lng,
                 'foto_laporan' => json_encode(['dummy_laporan.jpg']),
                 'prioritas_pelapor' => 'sedang',
                 'prioritas_admin' => 'sedang',
                 'status' => 'selesai',
-                'verified_by' => 1, // admin
-                'verified_at' => Carbon::now()->subDays(2),
-                'completed_at' => Carbon::now()->subHours(5),
+                'verified_by' => 1,
+                'verified_at' => Carbon::now()->subDays(rand(1, 10)),
+                'completed_at' => Carbon::now()->subHours(rand(1, 48)),
             ]);
         }
     }
