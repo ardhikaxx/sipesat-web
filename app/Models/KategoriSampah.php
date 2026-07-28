c91a2fe763d38fedfa44744a345daf96bc4165d1
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-28 19:00:15 +0700

    feat: pembaruan fitur app/Models/KategoriSampah.php

--- app/Models/KategoriSampah.php
+++ app/Models/KategoriSampah.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class KategoriSampah extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nama_kategori', 'deskripsi', 'icon', 'is_active'];
+    
 }
