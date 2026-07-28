027b753e78dc6813aafbbfcce007dcbc942f2d7e
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-28 19:00:15 +0700

    feat: pembaruan fitur app/Models/Berita.php

--- app/Models/Berita.php
+++ app/Models/Berita.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Berita extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['judul', 'slug', 'thumbnail', 'konten', 'kategori', 'penulis_id', 'status', 'tanggal_publish', 'views'];
+    protected $casts = ['tanggal_publish' => 'datetime'];
+    public function penulis() { return $this->belongsTo(User::class, 'penulis_id'); }
+    
 }
