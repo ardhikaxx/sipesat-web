c1b2ca89eba43641e29917c885b100a1b584fd9a
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-28 19:07:41 +0700

    fix: menambahkan LaporanSampahSeeder ke pemanggilan utama seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             DesaSeeder::class,
             KategoriSampahSeeder::class,
             UserSeeder::class,
+            LaporanSampahSeeder::class,
         ]);
     }
 }
