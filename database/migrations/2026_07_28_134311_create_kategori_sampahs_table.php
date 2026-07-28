c9c0bbf92f301e86d43acc7499b350ef0b2e46a2
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-28 19:00:21 +0700

    feat: penambahan skema database database/migrations/2026_07_28_134311_create_kategori_sampahs_table.php

---
+++ database/migrations/2026_07_28_134311_create_kategori_sampahs_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('kategori_sampahs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kategori', 100);
+            $table->text('deskripsi')->nullable();
+            $table->string('icon', 50)->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+    
+
+    public function down(): void
+    {
+        Schema::dropIfExists('kategori_sampahs');
+    }
+};
