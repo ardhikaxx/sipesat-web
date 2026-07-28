ee909fd4bf15a6c02c4e5266317df869a7c9c794
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-28 19:00:16 +0700

    feat: pembaruan fitur app/Models/Petugas.php

--- app/Models/Petugas.php
+++ app/Models/Petugas.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Petugas extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'nip', 'wilayah_tugas_kecamatan_id', 'status_petugas'];
+    public function user() { return $this->belongsTo(User::class); }
+    public function wilayahTugas() { return $this->belongsTo(Kecamatan::class, 'wilayah_tugas_kecamatan_id'); }
+    public function penugasans() { return $this->hasMany(Penugasan::class); }
+    
 }
